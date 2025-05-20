dbcffa4bcfc5034dda72db47c9967193253e00e9
Jose Bonilla <bonillasanchez98@gmail.com>
2025-05-19 21:24:59 -0300

    add: SOLID project

---
+++ SOLID/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SOLID
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*\Ejercicio 4: Aplicando SOLID
+             * Una empresa necesita un sistema de notificaciones que pueda enviar correos electrónicos y SMS, 
+             *pero la clase actual viola el Principio de Responsabilidad Única (SRP) porque también maneja el
+             *registro de logs*/
+
+            Console.WriteLine("********** Ejercicio 4: Aplicando SOLID **********");
+        }
+    }
+}
